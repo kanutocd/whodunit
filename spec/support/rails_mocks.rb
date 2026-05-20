5de6878842e80a6e8684b772810544a83f4ff613
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2026-05-20 13:07:46 +0800

    tests: no rails mocking neeeded

--- spec/support/rails_mocks.rb
+++ spec/support/rails_mocks.rb
@@ -1,28 +1,6 @@
 # frozen_string_literal: true
 
-# Mock Rails classes for testing when Rails isn't loaded
-unless defined?(Rails)
-  module Rails
-    class Railtie
-      def self.railtie_name(name = nil)
-        @railtie_name = name if name
-        @railtie_name
-      end
-
-      def self.initializer(name, **options, &block)
-        @initializers ||= []
-        @initializers << { name: name, options: options, block: block }
-      end
-
-      def self.initializers
-        @initializers || []
-      end
-    end
-  end
-
-  module ActiveSupport
-    def self.on_load(component, &)
-      # Mock - in real Rails this would call the block when the component is loaded
-    end
-  end
-end
+# activesupport and activerecord are runtime dependencies of whodunit and are
+# loaded unconditionally by lib/whodunit.rb via `require "active_support/all"`.
+# No hand-rolled Rails mocks are needed; this file is intentionally left
+# empty so that existing `require` calls in spec files continue to work.
